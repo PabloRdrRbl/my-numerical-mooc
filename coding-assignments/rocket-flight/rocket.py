cd9bbc6d331603ae0c7410ed2c47c95da5ecd474
pablordrrbl <prodrr05@estudiantes.unileon.es>
2016-07-07 23:59:08 +0200

    Starting firts coding assigment

---
+++ coding-assignments/rocket-flight/rocket.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+# Physical parameters
+# h --> the altitude of the rocket
+ms = 50  # mass of the rocket shell (kg)
+g = 9.81  # gravity acceleration (m/s^2)
+rho = 1.091  # average density, cte (kg/m^3)
+A = n
