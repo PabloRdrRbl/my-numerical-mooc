d34b811f1541cd929c81850dfa2bae4c50ae34e7
pablordrrbl <prodrr05@estudiantes.unileon.es>
2016-07-12 01:19:47 +0200

    Let's work 🛠

--- lessons/03_wave/03_01_ConservationLaw.py
+++ lessons/03_wave/03_01_ConservationLaw.py
@@ -7,6 +7,9 @@ rcParams['font.size'] = 16
 
 
 def rho_green_light(nx, rho_light):
+    """
+    This are the inital conditions of the problem
+    """
     rho = np.arange(nx) * 2. / nx * rho_light  # Before stop light
     rho[int((nx - 1) / 2):] = 0
 
@@ -28,3 +31,29 @@ plt.plot(x, rho, color='#003366', ls='-', lw=3)
 plt.ylabel('Traffic density')
 plt.xlabel('Distance')
 plt.ylim(-0.5, 11.)
+
+
+def computeF(u_max, rho_max, rho):
+    return u_max * rho * (1 - rho / rho_max)
+
+
+def ftbs(rho, nt, dt, dx, rho_max, u_max):
+    """Computes the solution with forward in time, backward in space
+    """
+    rho_n = np.zeros((nt, len(rho)))
+
+    rho_n[0, :] = rho.copy()
+
+    for t in range(1, nt):
+        F = computeF(u_max, rho_max, rho)
+        rho_n[t, 1:] = rho[1:] - dt / dx * (F[1:] - F[:-1])
+        rho_n[t, 0] = rho[0]
+        rho = rho_n[t].copy
+
+    return rho_n
+
+
+sigma = 1
+dt = sigma * dx
+
+rho_n = ftbs(rho, nt, dt, dx, rho_max, u_max)
